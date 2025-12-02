51f12d745942327a8e58174dd52664aec2cd9b42
namvt04 <vutiennam2004@gmail.com>
2025-12-02 08:38:40 +0700

    first commit

---
+++ src/main/java/it4341/HeTroGiup/HeTroGiupApplication.java
@@ -0,0 +1,13 @@
+package it4341.HeTroGiup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeTroGiupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeTroGiupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it4341/HeTroGiup/HeTroGiupApplicationTests.java
@@ -0,0 +1,13 @@
+package it4341.HeTroGiup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeTroGiupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
